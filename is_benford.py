f7a0cecd4a2c751c29e4f937d51a6ec1ec91737b
ayoubft <ayoubfatihi1999@gmail.com>
2021-04-20 13:43:38 +0000

    First commit

---
+++ is_benford.py
@@ -0,0 +1,6 @@
+"""
+This script is for functions that return the distribution of leading digits of a numerical distribution.
+Make sure you numerical distribution spans a few orders of magnitude.
+
+Author: ayoubft
+"""

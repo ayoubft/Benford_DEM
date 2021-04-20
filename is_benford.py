03dce9b28b3108a01294f944aac9d240dd5aec01
ayoubft <ayoubfatihi1999@gmail.com>
2021-04-20 14:04:19 +0000

    Update getdld

--- is_benford.py
+++ is_benford.py
@@ -10,4 +10,29 @@ def getdld(numd):
     Get the distribution of leading digits of a given numerical distribution
     """
     
+    f1, f2, f3, f4, f5, f6, f7, f8, f9 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     
+    for i in numd:
+        c = str(i)[0]
+        if c == '1':
+            f1 += 1
+        elif c == '2':
+            f2 += 1;
+        elif c == '3':
+            f3 += 1;
+        elif c == '4':
+            f4 += 1;
+        elif c == '5':
+            f5 += 1;
+        elif c == '6':
+            f6 += 1;
+        elif c == '7':
+            f7 += 1;
+        elif c == '8':
+            f8 += 1;
+        elif c == '9':
+            f9 += 1;
+        
+        
+    
+    return f1, f2, f3, f4, f5, f6, f7, f8, f9

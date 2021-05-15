0511897a276dcd4317476dd0038c929fbd30c740
ayoubft <ayoubfatihi1999@gmail.com>
2021-05-15 16:00:53 +0000

    Updated ipynb

--- benford_library.py
+++ benford_library.py
@@ -3,7 +3,7 @@ Leading Numbers Count
 This script is for functions that return the distribution of leading digits of a numerical distribution.
 Make sure you numerical distribution spans a few orders of magnitude.
 
-Author: ayoubft and __ferda_kech_kech
+Author: ayoubft & _ferda_kech_kech
 """
 
 # Import needed libraries
